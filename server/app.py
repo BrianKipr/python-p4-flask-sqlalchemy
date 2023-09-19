9f8e2c5b3c9aa4278cb5dcfcdd80efd4da10bee5
Briankipr <Brayos718@gmail.com>
2023-09-19 07:42:56 +0300

    assignment done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python3
 
+# app/app.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(port=5555)
+
+
